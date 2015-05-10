dfc3b034fef3942c208fc29aca640b3874f34334
Leo Martel <lmartel@github>
2015-05-10 00:38:36 -0700

    fixed github tokens

--- db/schema.rb
+++ db/schema.rb
@@ -40,9 +40,8 @@ ActiveRecord::Schema.define(version: 20150510072927) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
-    t.string   "github_token"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "full_name"
   end
 
