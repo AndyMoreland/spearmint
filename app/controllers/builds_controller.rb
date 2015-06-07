7066296df2936cbd42fc50f697e0d11ad11be4cc
Leo Martel <lmartel@github>
2015-06-07 16:25:55 -0700

    added issue deduping in build ui

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -3,27 +3,29 @@ require 'pp'
 class BuildsController < ApplicationController
   before_filter :load_project
   skip_before_action :verify_authenticity_token
-  
+
   def new
   end
 
   def create
     @build = @project.builds.build(params[:build].try(:permit, :title, :commit))
     @build.commit = @project.github_client.repo(@project.full_name).rels[:commits].get.data.first.sha
-    
+
     unless @build.save
       render error: "Can't find project with id #{params[:project_id]}"
     end
-    
+
     flash[:notice] = "Created build."
     redirect_to(@project)
   end
 
   def show
+    redirect_to(request.path, params: params, flash: { query: request.query_parameters } ) unless request.query_parameters.empty?
     @project_name = @project.name
     @build = @project.builds.find(params[:id])
+    @should_dedupe_issues = flash[:query] and flash[:query]['dedupe']
 
-    @all_issues = @build.issues.group_by(&:source).map { |source| 
+    @all_issues = @build.issues.group_by(&:source).map { |source|
       { source[0] => source[1].group_by(&:file) }
     }
   end
