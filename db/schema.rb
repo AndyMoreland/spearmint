acc562032da2047a5110ad2021ac75ecbb86574f
Leo Martel <lmartel@github>
2015-06-08 20:15:46 -0700

    added ignored_files defaults and testrunner backend now respects ignore list

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609001330) do
+ActiveRecord::Schema.define(version: 20150609004738) do
 
   create_table "builds", force: :cascade do |t|
     t.string   "title"
@@ -63,14 +63,14 @@ ActiveRecord::Schema.define(version: 20150609001330) do
   add_index "projects_users", ["user_id"], name: "index_projects_users_on_user_id"
 
   create_table "settings", force: :cascade do |t|
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",                                                    null: false
+    t.datetime "updated_at",                                                    null: false
     t.integer  "project_id"
-    t.integer  "user_with_token",             null: false
+    t.integer  "user_with_token",                                               null: false
     t.string   "build_command"
     t.string   "test_command"
-    t.integer  "concurrent_jobs", default: 2, null: false
-    t.text     "ignored_files"
+    t.integer  "concurrent_jobs", default: 2,                                   null: false
+    t.text     "ignored_files",   default: "[\"node_modules\",\"jquery*.js\"]"
   end
 
   add_index "settings", ["project_id"], name: "index_settings_on_project_id"
--- lib/test_runner/job_default.rb
+++ lib/test_runner/job_default.rb
@@ -1,11 +1,29 @@
+require 'json'
 require_relative 'job'
 
 module TestRunner
   class Job
     class Default < Job
-      
+
       def sources(project, commit, extension)
-        Dir[Rails.root.join 'clients', project.full_name, commit, '**', "*.#{extension}"]
+        begin
+          ignored = JSON.parse(project.setting.ignored_files || "[]").reject { |p| p.empty? }
+        rescue JSON::ParserError
+          ignored = []
+        else
+          ignored = [] unless ignored.is_a? Array
+        end
+        relatives = ignored.select { |p| p[0] == '/' }
+        globals = ignored - relatives
+
+        ignore_globs = (globals.map { |p| "./**/#{p}" }.concat relatives.map { |p| ".#{p}" }).map do |glob|
+          Rails.root.join 'clients', project.full_name, commit, glob
+        end
+
+        all_files = Dir[Rails.root.join 'clients', project.full_name, commit, '**', "*.#{extension}"]
+        ignored_files = ignore_globs.flat_map { |g| Dir[g] }
+
+        all_files - ignored_files
       end
 
       class << self
@@ -19,7 +37,7 @@ module TestRunner
           ObjectSpace.each_object(self.singleton_class).to_a.reject { |k| k == Default }
         end
       end
-      
+
     end
   end
 end
